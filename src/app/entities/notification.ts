9123a45c1a5b09cc42de5f99bf6a1005abdaea35
Lucas <lucasbiazonpalma@gmail.com>
2024-02-15 18:09:14 -0300

    Entidade de notificação

---
+++ src/app/entities/notification.ts
@@ -0,0 +1,45 @@
+export interface NotificatonProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notificaton {
+  private props: NotificatonProps;
+  constructor(props: NotificatonProps) {
+    this.props = props;
+  }
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: string) {
+    this.props.content = content;
+  }
+  public get content(): string {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
